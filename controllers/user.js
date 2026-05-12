b17775520439925c4317f896f3d9d10264e997a2
sarthakshruti999-code <sarthakshruti999@gmail.com>
2026-05-12 08:59:40 +0530

    fix: use setPassword instead of register

--- controllers/user.js
+++ controllers/user.js
@@ -1,64 +1,40 @@
 const User = require("../models/user");
 
-// SIGNUP FORM
 module.exports.renderSignupForm = (req, res) => {
-    return res.render("user/signup");
+    res.render("user/signup");
 };
 
-// SIGNUP
 module.exports.signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
-
-        const newUser = new User({
-            email,
-            username,
-        });
-
-        // REGISTER USER
-        const registeredUser = await User.register(newUser, password);
-
-        // LOGIN AFTER SIGNUP
-        req.login(registeredUser, (err) => {
-            if (err) {
-                return next(err);
-            }
-
+        const newUser = new User({ username, email });
+        await newUser.setPassword(password);
+        await newUser.save();
+        req.login(newUser, (err) => {
+            if (err) return next(err);
             req.flash("success", "Welcome to Wanderlust!");
-
-            return res.redirect("/listings");
+            res.redirect("/listings");
         });
-
     } catch (err) {
         req.flash("error", err.message);
-
-        return res.redirect("/users/signup");
+        res.redirect("/users/signup");
     }
 };
 
-// LOGIN FORM
 module.exports.renderLoginForm = (req, res) => {
-    return res.render("user/login");
+    res.render("user/login");
 };
 
-// LOGIN
 module.exports.login = (req, res) => {
     req.flash("success", "Welcome back!");
-
     const redirectUrl = res.locals.redirectUrl || "/listings";
-
-    return res.redirect(redirectUrl);
+    res.redirect(redirectUrl);
 };
 
-// LOGOUT
 module.exports.logout = (req, res, next) => {
     req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-
+        if (err) return next(err);
         req.flash("success", "Logged out successfully!");
-
-        return res.redirect("/users/login");
+        res.redirect("/users/login");
     });
 };
